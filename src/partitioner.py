fixed wrong loss calculation for splitting

src/partitioner.py
@@ -139,8 +139,8 @@ class DecisionTreePartitioner(AbstractPartitioner):
         variance_grid_points = self.variances[indices, :]
         mean_variances = np.mean(variance_grid_points, axis=0)
 
-        # pointwise_l2_loss = (variance_grid_points - mean_variances) ** 2
-        pointwise_l2_loss = (self.variances - mean_variances) ** 2
+        pointwise_l2_loss = (variance_grid_points - mean_variances) ** 2
+        # pointwise_l2_loss = (self.variances - mean_variances) ** 2
         loss_sum = np.sum(pointwise_l2_loss, axis=None)
 
         return loss_sum.item()

test/test_partitioner.py
@@ -53,7 +53,7 @@ class TestPartitioner(unittest.TestCase):
         plt.show()
 
         # create color map
-        color_map = np.asarray(['THis is a long string' for i in range(indices.shape[1])])
+        color_map = np.asarray(['This is a long string' for i in range(indices.shape[1])])
         for i, index in enumerate(indices):
             color_map[index] = colors[i]
         args = {'c': list(color_map)}