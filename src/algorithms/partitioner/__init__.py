fixed mmd and nll computation

src/algorithms/partitioner/__init__.py
@@ -63,8 +63,9 @@ class Region:
         # regions pdp estimate:
         pdp_y_points = np.mean(self.y_points, axis=0)
         pdp_y_variances = np.mean(self.y_variances, axis=0)
+        pdp_y_std = np.sqrt(pdp_y_variances)
 
-        log_prob = norm.logpdf(true_y, loc=pdp_y_points, scale=pdp_y_variances)
+        log_prob = norm.logpdf(true_y, loc=pdp_y_points, scale=pdp_y_std)
         result = - np.mean(log_prob)
         return result
 

src/sampler/__init__.py
@@ -9,7 +9,7 @@ from sklearn.gaussian_process.kernels import RBF
 
 from src.utils.plotting import Plottable, get_ax, check_and_set_axis
 from src.utils.typing import ColorType
-from src.utils.utils import config_list_to_array, get_hyperparameters, median_distance_between_points
+from src.utils.utils import config_list_to_array, get_hyperparameters, median_distance_between_points, unscale
 
 
 class Sampler(Plottable, ABC):
@@ -67,7 +67,8 @@ class Sampler(Plottable, ABC):
         X = np.concatenate((X_samples, X_uniform))
 
         # Calculate
-        median_l2 = median_distance_between_points(X_uniform)
+        X_uniform_unscaled = unscale(X_uniform, self.config_space)
+        median_l2 = median_distance_between_points(X_uniform_unscaled)
         rbf = RBF(median_l2)
 
         covariances = rbf(X)

src/utils/utils.py
@@ -109,9 +109,10 @@ def get_uniform_distributed_ranges(cs: CS.ConfigurationSpace,
     return res
 
 
-def median_distance_between_points(X:np.ndarray) -> float:
+def median_distance_between_points(X: np.ndarray) -> float:
     X = np.expand_dims(X, axis=0)
     dif = X - X.transpose((1, 0, 2))
     dif_2 = np.sum(np.square(dif), axis=2)
     distances = np.sqrt(dif_2)
-    return np.median(distances[distances != 0]).item()
+    median = np.median(distances[distances != 0]).item()
+    return median